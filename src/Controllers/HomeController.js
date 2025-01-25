9bbe8aa25a8dc6ae3c1528407910027c0bbf1474
Twimm <hicli0001@gmail.com>
2025-01-25 17:21:59 +0100

    adding dashboard ui

--- src/Controllers/HomeController.js
+++ src/Controllers/HomeController.js
@@ -10,6 +10,11 @@ class HomeController {
         res.render('widget', { title: pageTitle });
     }
 
+    static serveAdminPage(req, res) {
+        const pageTitle = "Dashboard !!";
+        res.render('admin', { title: pageTitle });
+    }
+
     static getViewer(req, res) {
         const pageTitle = "Viewer !!";
         res.render('viewer', { title: pageTitle });
--- src/Routes/HomeRoutes.js
+++ src/Routes/HomeRoutes.js
@@ -7,8 +7,7 @@ const router = express.Router();
 // Define home route
 router.get('/', HomeController.getHomePage);
 router.get('/serve-widget', HomeController.serveWidget);
-
-
+router.get('/dashboard', HomeController.serveAdminPage);
 router.get('/viewer', HomeController.getViewer);
 
 module.exports = router;
